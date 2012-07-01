04960451b64f21743c52388b3040b8cffd858091
Chris Langford <chris@hellocld.com>
2012-06-30 21:30:52 -0400

    Added for the SimpleMoveSystem

---
+++ com/hellocld/AGED/basicComponents/SimpleMove.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.hellocld.AGED.basicComponents;
+
+import com.hellocld.AGED.core.Component;
+
+/**
+ * Triggers the SimpleMoveSystem
+ * 
+ * @author CLD
+ *
+ */
+public class SimpleMove implements Component {
+	//boolean to keep track of whether or not it should be moving
+	public boolean active = true;
+}
---
+++ com/hellocld/AGED/basicComponents/Velocity2D.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.hellocld.AGED.basicComponents;
+
+import com.hellocld.AGED.core.Component;
+
+/**
+ * A two-dimensional velocity component
+ * @author CLD
+ *
+ */
+public class Velocity2D implements Component {
+	public float xVel = 0;
+	public float yVel = 0;
+}
