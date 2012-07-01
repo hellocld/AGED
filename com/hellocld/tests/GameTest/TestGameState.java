62d876ac54fe42d1d6c9f4e1e8a9397d8d0dd677
Chris Langford <chris@hellocld.com>
2012-06-30 21:33:17 -0400

    Updated the TestGameState so there's two entities, and one of them MOVES!

--- com/hellocld/tests/GameTest/TestGameState.java
+++ com/hellocld/tests/GameTest/TestGameState.java
@@ -16,23 +16,33 @@ public class TestGameState extends GameState {
 	public void create() {
 		super.create();
 		
-		//add a test entity with a bunch of components
-		int testEntity = em.createEntity();
+		//add a couple test entities with a bunch of components
+		int testEntity1 = em.createEntity();
+		em.addComponent(testEntity1, new Position2D());
+		em.addComponent(testEntity1, new Size2D());
+		em.addComponent(testEntity1, new Render());
+		em.getComponent(testEntity1, Position2D.class).x = 10;
+		em.getComponent(testEntity1, Position2D.class).y = 10;
+		em.getComponent(testEntity1, Size2D.class).width = 20;
+		em.getComponent(testEntity1, Size2D.class).height = 20;
 		
-		//debug - print the testEntity's int value
-		//System.out.println("testEntity: "+testEntity);
-		
-		em.addComponent(testEntity, new Position2D());
-		em.addComponent(testEntity, new Size2D());
-		em.addComponent(testEntity, new Render());
-		em.getComponent(testEntity, Position2D.class).x = 10;
-		em.getComponent(testEntity, Position2D.class).y = 10;
-		em.getComponent(testEntity, Size2D.class).width = 20;
-		em.getComponent(testEntity, Size2D.class).height = 20;
+		int testEntity2 = em.createEntity();
+		em.addComponent(testEntity2, new Position2D());
+		em.addComponent(testEntity2, new Size2D());
+		em.addComponent(testEntity2, new Render());
+		em.addComponent(testEntity2, new SimpleMove());
+		em.addComponent(testEntity2, new Velocity2D());
+		em.getComponent(testEntity2, Position2D.class).x = 60;
+		em.getComponent(testEntity2, Position2D.class).y = 83;
+		em.getComponent(testEntity2, Size2D.class).width = 90;
+		em.getComponent(testEntity2, Size2D.class).height = 17;
+		em.getComponent(testEntity2, Velocity2D.class).xVel = 1;
+		em.getComponent(testEntity2, Velocity2D.class).yVel = -1;
 		
 		//add any Systems to the systems list
 		//Be sure to add them in the order you want them to execute
 		systems.add(new Render2DSystem());
+		systems.add(new SimpleMoveSystem());
 	}
 	
 	public void update() {
