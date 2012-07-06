9d3d6cec005ceb7d5b531ee6601486909cc05e62
Chris Langford <chris@hellocld.com>
2012-07-06 16:29:02 -0400

    Reduced memory useage, no longer constantly creates new instances of variables

--- com/hellocld/AGED/basicSystems/SimpleMoveSystem.java
+++ com/hellocld/AGED/basicSystems/SimpleMoveSystem.java
@@ -17,16 +17,18 @@ import com.hellocld.AGED.core.EntityManager;
  *
  */
 public class SimpleMoveSystem implements ASystem {
-
+	Set<Integer> moveSet;
+	Iterator<Integer> moveIter;
+	int entity;
 	/* (non-Javadoc)
 	 * @see com.hellocld.AGED.core.ASystem#execute(com.hellocld.AGED.core.EntityManager)
 	 */
 	@Override
 	public void execute(EntityManager em) {
-		Set<Integer> moveSet = em.getAllEntitiesPossessingComponent(SimpleMove.class);
+		moveSet = em.getAllEntitiesPossessingComponent(SimpleMove.class);
 		
-		for(Iterator<Integer> moveIter = moveSet.iterator(); moveIter.hasNext();) {
-			int entity = moveIter.next();
+		for(moveIter = moveSet.iterator(); moveIter.hasNext();) {
+			entity = moveIter.next();
 			if(em.getComponent(entity, SimpleMove.class).active){
 				em.getComponent(entity, Position2D.class).x += em.getComponent(entity, Velocity2D.class).xVel;
 				em.getComponent(entity, Position2D.class).y += em.getComponent(entity, Velocity2D.class).yVel;
