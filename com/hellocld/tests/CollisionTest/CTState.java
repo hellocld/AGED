cadbefc2f036d6eb72d5c306bbee5c0d4863e84c
Chris Langford <chris@hellocld.com>
2012-07-12 13:40:18 -0400

    More bouncy things. Also removed the old boring crap from CTState

--- com/hellocld/tests/CollisionTest/CTState.java
+++ com/hellocld/tests/CollisionTest/CTState.java
@@ -6,15 +6,15 @@ package com.hellocld.tests.CollisionTest;
 import com.hellocld.AGED.basicComponents.Collision2D;
 import com.hellocld.AGED.basicComponents.Position2D;
 import com.hellocld.AGED.basicComponents.Render;
-import com.hellocld.AGED.basicComponents.SimpleMove;
 import com.hellocld.AGED.basicComponents.Size2D;
 import com.hellocld.AGED.basicComponents.Velocity2D;
 import com.hellocld.AGED.basicSystems.Collision2DSystem;
 import com.hellocld.AGED.basicSystems.Render2DSystem;
 import com.hellocld.AGED.basicSystems.SimpleMoveSystem;
 import com.hellocld.AGED.core.GameState;
-import com.hellocld.tests.CollisionTest.ASystems.CollisionMarkerCreatorSystem;
-import com.hellocld.tests.CollisionTest.ASystems.CollisionMarkerHandlerSystem;
+import com.hellocld.tests.CollisionTest.ASystems.BounceSurfaceHandler;
+import com.hellocld.tests.CollisionTest.ASystems.BouncingBoxGenerator;
+import com.hellocld.tests.CollisionTest.Components.BounceSurface;
 
 /**
  * @author CLD
@@ -27,52 +27,42 @@ public class CTState extends GameState {
 	public void create() {
 		super.create();
 		
-		entity1 = em.createEntity();
-		entity2 = em.createEntity();
+		//let's make some walls
+		int[] walls = new int[4];
 		
-		em.addComponent(entity1, new Position2D());
-		em.addComponent(entity1, new Size2D());
-		em.addComponent(entity1, new Velocity2D());
-		em.addComponent(entity1, new Collision2D());
-		em.addComponent(entity1, new SimpleMove());
-		em.addComponent(entity1, new Render());
+		for(int i = 0; i<4; i++) {
+			walls[i] = em.createEntity();
+			em.addComponent(walls[i], new Position2D());
+			em.addComponent(walls[i], new Size2D());
+			em.addComponent(walls[i], new Collision2D());
+			em.addComponent(walls[i], new Velocity2D());
+			em.addComponent(walls[i], new BounceSurface());
+			em.addComponent(walls[i], new Render());
+		}
 		
-		em.addComponent(entity2, new Position2D());
-		em.addComponent(entity2, new Size2D());
-		em.addComponent(entity2, new Velocity2D());
-		em.addComponent(entity2, new Collision2D());
-		em.addComponent(entity2, new SimpleMove());
-		em.addComponent(entity2, new Render());
-		
-		em.getComponent(entity1, Position2D.class).x = 0;
-		em.getComponent(entity1, Size2D.class).width = 20;
-		em.getComponent(entity1, Size2D.class).height = 20;
-		em.getComponent(entity1, Velocity2D.class).xVel = 5;
-		em.getComponent(entity2, Position2D.class).x = 300;
-		em.getComponent(entity2, Position2D.class).y = 10;
-		em.getComponent(entity2, Size2D.class).width = 20;
-		em.getComponent(entity2, Size2D.class).height = 20;
-		em.getComponent(entity2, Velocity2D.class).xVel = -2;
+		em.getComponent(walls[0], Position2D.class).setPosition(0, 0);
+		em.getComponent(walls[0], Size2D.class).setSize(319,1);
+		em.getComponent(walls[0], BounceSurface.class).yAxis = true;
+		em.getComponent(walls[1], Position2D.class).setPosition(319, 0);
+		em.getComponent(walls[1], Size2D.class).setSize(1,239);
+		em.getComponent(walls[1], BounceSurface.class).xAxis = true;
+		em.getComponent(walls[2], Position2D.class).setPosition(1, 239);
+		em.getComponent(walls[2], Size2D.class).setSize(319,1);
+		em.getComponent(walls[2], BounceSurface.class).yAxis = true;
+		em.getComponent(walls[3], Position2D.class).setPosition(0, 1);
+		em.getComponent(walls[3], Size2D.class).setSize(1,239);
+		em.getComponent(walls[3], BounceSurface.class).xAxis = true;
 		
 		systems.add(new Render2DSystem());
-		systems.add(new SimpleMoveSystem());
+		systems.add(new BouncingBoxGenerator());
 		systems.add(new Collision2DSystem());
-		systems.add(new CollisionMarkerCreatorSystem());
-		systems.add(new CollisionMarkerHandlerSystem());
+		systems.add(new BounceSurfaceHandler());
+		systems.add(new SimpleMoveSystem());
 		
 	}
 	
 	public void update() {
 		super.update();
-		
-		if(em.getComponent(entity1, Position2D.class).x > 300) {
-			em.getComponent(entity1, Position2D.class).x = 0;
-		}
-		
-		if(em.getComponent(entity2, Position2D.class).x < 0) {
-			em.getComponent(entity2, Position2D.class).x = 300;
-		}
-		
 		
 	}
 }
---
+++ com/hellocld/tests/CollisionTest/Components/BounceSurface.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.hellocld.tests.CollisionTest.Components;
+
+import com.hellocld.AGED.core.Component;
+
+/**
+ * @author CLD
+ *
+ */
+public class BounceSurface implements Component {
+	public boolean xAxis = false;
+	public boolean yAxis = false;
+}
