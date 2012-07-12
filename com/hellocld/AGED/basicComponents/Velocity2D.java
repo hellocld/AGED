24f2ed59a0d731edbb2d827e4817d5475195c2fb
Chris Langford <chris@hellocld.com>
2012-07-12 08:09:26 -0400

    Added setVelocity method

--- com/hellocld/AGED/basicComponents/Velocity2D.java
+++ com/hellocld/AGED/basicComponents/Velocity2D.java
@@ -13,4 +13,15 @@ import com.hellocld.AGED.core.Component;
 public class Velocity2D implements Component {
 	public float xVel = 0;
 	public float yVel = 0;
+	
+	
+	/**
+	 * An easy way to set the x and y velocity values
+	 * @param xVel	The x axis velocity
+	 * @param yVel	The y axis velocity
+	 */
+	public void setVelocity(float xVel, float yVel) {
+		this.xVel = xVel;
+		this.yVel = yVel;
+	}
 }
