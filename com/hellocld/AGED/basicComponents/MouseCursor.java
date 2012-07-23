5ff71242ed6ccc4adacf0cb8878f4a750b39e7ee
Chris Langford <chris@hellocld.com>
2012-07-23 13:29:44 -0400

    Created a component for mouse cursors

---
+++ com/hellocld/AGED/basicComponents/MouseCursor.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.hellocld.AGED.basicComponents;
+
+import com.hellocld.AGED.core.Component;
+
+/**
+ * A simple component that lets the MouseCursorSystem know what entity to manipulate
+ * @author CLD
+ *
+ */
+public class MouseCursor implements Component {
+	public boolean leftButton = false;
+	public boolean rightButton = false;
+}
