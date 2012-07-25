066781fe33cebf6b1b92956fa8f0a4678d3f05f1
Chris Langford <chris@hellocld.com>
2012-07-25 13:15:10 -0400

    Some components for identifying ball and paddle entities

---
+++ com/hellocld/AGEDpong/Components/BallMove.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.hellocld.AGEDpong.Components;
+
+import com.hellocld.AGED.core.Component;
+
+/**
+ * Component for the ball
+ * @author CLD
+ *
+ */
+public class BallMove implements Component {
+
+}
---
+++ com/hellocld/AGEDpong/Components/PlayerMove.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.hellocld.AGEDpong.Components;
+
+import com.hellocld.AGED.core.Component;
+
+/**
+ * Component for the Player Paddle
+ * @author CLD
+ *
+ */
+public class PlayerMove implements Component {
+
+}
