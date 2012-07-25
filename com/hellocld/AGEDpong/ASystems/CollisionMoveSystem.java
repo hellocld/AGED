d6908496326c826d9b50e11becf70310d86e389b
Chris Langford <chris@hellocld.com>
2012-07-25 13:15:58 -0400

    Empty shells of systems, not started yet

---
+++ com/hellocld/AGEDpong/ASystems/CollisionMoveSystem.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.hellocld.AGEDpong.ASystems;
+
+import com.hellocld.AGED.core.ASystem;
+import com.hellocld.AGED.core.EntityManager;
+import com.hellocld.AGED.core.Game;
+
+/**
+ * @author atto
+ *
+ */
+public class CollisionMoveSystem implements ASystem {
+
+	/* (non-Javadoc)
+	 * @see com.hellocld.AGED.core.ASystem#execute(com.hellocld.AGED.core.Game, com.hellocld.AGED.core.EntityManager)
+	 */
+	@Override
+	public void execute(Game game, EntityManager em) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
